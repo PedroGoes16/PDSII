71be861ed6cd0f62631ef339f000370d249f1bb0
Pedro <pedrohenriquesk10@yahoo.com>
2021-08-04 22:39:48 -0300

    Studying

---
+++ Teste/BST.cpp
@@ -0,0 +1,45 @@
+#include "BST.hpp"
+NodeT* createNode(int data) {
+    NodeT* aux = new NodeT;
+    aux->data = data;
+    aux->esq = nullptr;
+    aux->dir = nullptr;
+    return aux;
+}
+
+void BST::insertNodeHelper(NodeT* n, int data) 
+{
+    if (data < n->data) {
+        if (n->esq == nullptr) {
+            n->esq = createNode(data);
+        } else {
+            insertNodeHelper(n->esq, data);
+        }
+    } else if (data > n->data) {
+        if (n->dir == nullptr) {
+            n->dir = createNode(data);
+        } else {
+            insertNodeHelper(n->dir, data);
+        }
+    }
+}
+
+void BST::insertNode(int data) {
+    if (root != nullptr) {
+        insertNodeHelper(root, data);
+    } else {
+        root = createNode(data);
+    }
+}
+
+void inorder(NodeT* n) {
+    if(n == nullptr)
+    return;
+    inorder(n->esq);
+    cout << n->data << " ";
+    inorder(n->dir);
+}
+void BST::display() {
+    inorder(root);
+    cout << endl;
+}
---
+++ Teste/main.cpp
@@ -0,0 +1,12 @@
+#include "BST.hpp"
+int main() {
+    BST bst;
+    bst.insertNode(5);
+    bst.insertNode(2);
+    bst.insertNode(7);
+    bst.insertNode(6);
+
+    bst.display();
+
+    return 0;
+}
